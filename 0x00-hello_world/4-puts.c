d6ab769d7f0fb876ea01c7ff5bdb11c71d1fef6d
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-10 15:06:09 +0000

    printing a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+*main function print a string
+*always return 0
+*/
+
+int main(void)
+{
+puts("Programming is like building a multillingual puzzle", stdout);
+return (0);
+}
