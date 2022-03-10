fc8c8fb3615f2bfd42cb3f7cddac5d3ffd6cee42
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-10 18:39:09 +0000

    trailing spaces removed as per betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char s[c] = "\"Programming is like building a multilingual puzzle";
+char s[] = "\"Programming is like building a multilingual puzzle";
 puts(s);
 return (0);
 }
