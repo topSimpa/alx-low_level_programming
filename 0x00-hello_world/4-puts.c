b7f14e6681f4e3dd159a19e0df68fe120dafe427
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-10 18:56:15 +0000

    trailing spaces removed as per betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-*main function print a string
-*always return 0
+/**
+*main - Entry point
+*Return: Always 0 (Success)
 */
-
 int main(void)
 {
 char s[] = "\"Programming is like building a multilingual puzzle";
