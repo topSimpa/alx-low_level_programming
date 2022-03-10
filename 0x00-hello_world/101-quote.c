126b1a8c9b3e0731863780d08e7698dc97cd8300
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-10 21:32:36 +0000

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char s[ ] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fputs(s, stderr);
+return (1);
+}
