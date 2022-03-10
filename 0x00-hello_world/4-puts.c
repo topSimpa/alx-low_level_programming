928424b4924014143cffeeec450092c2bd117aff
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-10 16:14:57 +0000

    using puts function to print to screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {  
-char s[] = "\"Programming is like building a multillingual puzzle"; 
+char s[] = "\"Programming is like building a multilingual puzzle"; 
 puts(s);
 return (0);
 }
