cf965ceb2bba1b75076538695c3160dde1ebcd08
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-12 01:35:24 +0000

    printing the numbers using putchar and ascii code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Print out the lower-case of the english alphabet
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int num;
+
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+putchar(10);
+
+return (0);
+}
