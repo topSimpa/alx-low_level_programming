ed74f6979f7cf3decd0798ebfc2925ba3d92ab00
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-12 05:15:59 +0000

    creating a 2 2-digit number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Print out the 2 2-digit number
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int num, m, t, y;
+
+for (num = 48; num <= 57; num++)
+{
+for (m = 48; m <= 57; m++)
+{
+
+for (t = 48; t <= 57; t++)
+{
+for (y = 48; y <= 57; y++)
+{
+putchar(num);
+putchar(m);
+putchar(32);
+putchar(t);
+putchar(y);
+if (num == 57 && m == 57 && t == 57 && y == 57)
+{
+break;
+}
+else
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar(10);
+
+return (0);
+}
