8ca876793cd938b7f2e385e8108a4899afe8d8c2
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-15 20:29:50 +0000

    commiting main.h and _putchar and README

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main Entry-point
+* always printing _putchar
+* Return: Always 0 (sucess)
+*/
+
+int main (void)
+{
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putchar(114);
+putchar(10);
+
+return (10);
+}
