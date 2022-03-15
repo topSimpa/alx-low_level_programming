f5f147e8adf7b5d28ee39863136c546da6756f60
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-15 22:01:48 +0000

    defining my own function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet - Used to Print lower case alphabet
+* Return: Void (Sucess)
+*/
+
+
+void print_alphabet(void)
+{
+
+int n = 97;
+
+while (n < 123)
+{
+_putchar(n);
+n++;
+}
+_putchar(10);
+
+}
