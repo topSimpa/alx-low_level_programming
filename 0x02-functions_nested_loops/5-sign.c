f50ae2a486fe308fbf93f7aed5d202a3764a4ef5
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 00:20:52 +0000

    change name into sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 
 if (n  == 0)
 {
-return (0);
 putchar(48);
+return (0);
 }
 else if (n > 0)
 {
-return (1);
 putchar(43);
+return (1);
 }
 else
 {
-return (-1);
 putchar(45);
-
+return (-1);
 }
+putchar(10)
 }
