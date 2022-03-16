0b8136b6c5463e6f6f24c416f72656663fd592bb
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 04:32:07 +0000

    creating nine times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* times_table - Used to Print lower case alphabet 10 times
+* Return: Void (Sucess)
+*/
+
+
+void times_table(void)
+{
+
+int r = 0;
+while (r < 10)
+{
+
+int e = 0;
+
+while (e < 10)
+{
+if (e * r > 10)
+{
+_putchar((e * r) / 10 + '0');
+_putchar((e * r) % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar((e * r) + '0');
+_putchar(',');
+_putchar(' ');
+}
+e++;
+}
+_putchar(10)
+r++;
+}
+}
