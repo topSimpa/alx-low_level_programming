78315025f609995e141b2d025aa3fe4eb6242ad6
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 03:56:14 +0000

    jack_bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,14 +15,14 @@ while (r < 24)
 {
 int e = 0;
 
-_putchar(r / 10);
-_putchar(r % 10);
+_putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
 _putchar (':');
 
 while (e < 60)
 {
-_putchar (e / 10);
-_putchar (e % 10);
+_putchar (e / 10 + '0');
+_putchar (e % 10 + '0');
 e++;
 }
 putchar(10);
