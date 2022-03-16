2cbdfd9ad848364e727023c9ddec57c5847ebd50
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 01:59:05 +0000

    making input an absolute value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,33 @@
 
 /**
 * print_last_digit - To check  sign of number
-* @n: n is the character to check for sign
-* Return: int (Sucess)
+* @n: n is the character to  print last digit
+*Return: int (Sucess)
 */
 
 
 int print_last_digit(int n)
 {
-int a = _abs(n);
+
+if (n  == 0)
+{
+int a = n;
 int r = a % 10;
 _putchar(r + '0');
 return (r);
-
+}
+else if (n > 0)
+{
+int a = n;
+int r = a % 10;
+_putchar(r + '0');
+return (r);
+}
+else
+{
+int a = -1 * n;
+int r = a % 10;
+_putchar(r + '0');
+return (r);
+}
 }
