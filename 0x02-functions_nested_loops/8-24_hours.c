701fcbe16f970583bae66c985fc4d8b4410ee29d
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 03:47:34 +0000

    jack_bauer time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* jack_bauer - Used to Print lower case alphabet 10 times
+* Return: Void (Sucess)
+*/
+
+
+void jack_bauer(void)
+{
+
+int r = 0;
+while (r < 24)
+{
+_putchar(r / 10);
+_putchar(r % 10);
+_putchar (':')
+
+int e = 0;
+
+while (e < 60)
+{
+_putchar (e / 10);
+_putchar (e % 10);
+e++;
+}
+putchar(10);
+r++;
+}
