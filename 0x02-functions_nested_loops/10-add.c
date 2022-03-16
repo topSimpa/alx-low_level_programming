91e0d5513bf98c66f6bc390cbaa4181f1b98b933
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-16 21:48:25 +0000

    creating a script that adds two numbers up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - adds two numbers together
+*@a: the first int to be added
+*@b: the second int to be added
+* Return: int the result from addition (Sucess)
+*/
+
+
+int add(int a, int b)
+{
+
+int n = a + b;
+return (n);
+
+}
