cba827a9c16c589d0fb5e4b3845f66bacd3b3795
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-17 00:02:14 +0000

    adding a new function to do the writing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,47 @@
 
 void print_to_98(int n)
 {
-int i;
-for (i = n; i < 99; i++)
+
+if (n > 98)
+{
+while (n > 97)
+{
+write_num(n);
+n--;
+}
+}
+else
+{
+while (n < 99)
+{
+write_num(n);
+n++;
+}
+}
+_putchar(10);
+}
+
+/**
+* write_num - to write out number
+* @i: number to print
+* Return: void (Sucess)
+*/
+
+void write_num(int i)
 {
 if (i > 9)
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
+if (i == 98)
+{
+break;
+}
 }
 else
 {
 _putchar(i + '0');
-}
 _putchar(',');
 _putchar(' ');
 }
-_putchar(10);
 }
