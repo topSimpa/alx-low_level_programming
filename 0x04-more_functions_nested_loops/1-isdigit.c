eee4ef4d34b430f6df076e1367b3f52309887e44
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-18 03:37:27 +0000

    creating a script that checks if a number is just a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isupper - To check if lower case
+* @c: c is the character to check for case
+* Return: int (Sucess)
+*/
+
+
+int _isupper(int c)
+{
+
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+
+}
+}
