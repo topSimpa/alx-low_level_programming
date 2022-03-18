401d54f5872203b5ef95c37cb4f465ec1b001e10
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-18 04:42:21 +0000

    prints 0-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - To print out 0-14
+* Print out the numbers in decimal
+* Return: void (Success)
+*/
+
+void more_numbers(void)
+{
+int i, num;
+num = 0;
+i = 0;
+x
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num > 9)
+{
+_putchar(num / 10 + '0');
+}
+_putchar(num % 10 + '0');
+}
+_putchar(10);
+
+}
+}
