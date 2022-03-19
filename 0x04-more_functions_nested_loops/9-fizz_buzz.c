44d32839bebb2ebe94b4662843e8e8eaf02664e4
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-19 01:25:45 +0000

    fizz buzz printing

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Print out a  1-100
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+for (int num = 1; num < 101; num++)
+{
+if (num % 3 == 0 && num % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (num % 5 == 0)
+{
+printf("Buzz");
+}
+else if (num % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+if (num > 9)
+{
+putchar(num / 10 + '0');
+}
+else if (num > 99)
+{
+putchar(num / 100 + '0');
+putchar((num / 100) % 10 + '0');
+}
+putchar(num % 10 + '0');
+}
+if (num != 100)
+{
+putchar (' ');
+}
+}
+putchar(10);
+}
