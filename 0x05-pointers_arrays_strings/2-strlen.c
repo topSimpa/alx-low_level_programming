06b3145808f5e8f911543928685ffc88bae75cff
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-21 19:33:53 +0000

    returning the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - change all input to 98
+* @s: s is the pointer to an array of char (string)
+* Return : int return length of string (success)
+*/
+
+int _strlen(char *s)
+{
+int r = sizeof(s);
+return (r);
+}
