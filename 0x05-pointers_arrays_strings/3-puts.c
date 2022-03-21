a08edbfce9730610e91dd7163ad42d4e29026a66
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-21 21:35:40 +0000

    re engineer the puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - print a string
+* @s: s is the string pointer (string)
+* Return: void (success)
+*/
+
+void _puts(char *s)
+{
+int i = 0;
+while (1)
+{
+if (s[i] == '\0')
+{
+break;
+}
+_putchar(s[i]);
+i++;
+}
+}
+
