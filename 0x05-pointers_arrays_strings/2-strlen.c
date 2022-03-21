65854628e4d96156cf9f08a78747599524c5620d
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-21 19:48:35 +0000

    trying out the adress of the pointer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,11 @@
 /**
 * _strlen - change all input to 98
 * @s: s is the pointer to an array of char (string)
-* Return : int return length of string (success)
+* Return: int return length of string (success)
 */
 
 int _strlen(char *s)
 {
-int r = sizeof(*s);
+int r = sizeof(&s);
 return (r);
 }
