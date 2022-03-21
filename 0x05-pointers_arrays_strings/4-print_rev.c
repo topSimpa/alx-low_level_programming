dbe79bae307edaa88b23d3b712a713a8ae6ccd4e
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-21 22:07:24 +0000

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - print a string in reverse
+* @s: s is the string pointer (string)
+* Return: void (success)
+*/
+
+void print_rev(char *s)
+{
+int i = _strlen(s);
+while (i > 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
+
+/**
+* _strlen - return the length of a string
+* @s: s is the pointer to an array of char (string)
+* Return: int return length of string (success)
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (1)
+{
+if (s[i] == '\0')
+{
+break;
+}
+i++;
+}
+return (i);
+}
+
