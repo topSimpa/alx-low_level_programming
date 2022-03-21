6e7b4ff2eee6b07c4ccbb349e2663085d733f53c
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-21 19:07:11 +0000

    using pointers to change variable value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - change all input to 98
+* @n: n is the pointer to a interger type
+* Return : Always 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
