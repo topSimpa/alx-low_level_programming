c2b43c1ead3d90feb4a9250408d36eeb44b6ac6f
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-22 01:00:33 +0000

    puttinh len function before half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,37 +2,6 @@
 #include <stdio.h>
 
 /**
-* puts_half - print half of a string
-* @s: s is the string pointer (string)
-* Return: void (success)
-*/
-
-void puts_half(char *s)
-{
-int len = _strlen(s);
-
-if (len %  2 == 0)
-{
-int r = len / 2;
-while (r < r + r)
-{
-_putchar(s[r]);
-r++;
-}
-}
-else
-{
-int e = len + 1 / 2;
-while (e < e + (e - 1))
-{
-_putchar(s[e]);
-e++;
-}
-}
-_putchar('\n');
-}
-
-/**
 * _strlen - change all input to 98
 * @s: s is the pointer to an array of char (string)
 * Return: int return length of string (success)
@@ -52,3 +21,36 @@ i++;
 return (i);
 }
 
+/**
+* puts_half - print half of a string
+* @s: s is the string pointer (string)
+* Return: void (success)
+*/
+
+void puts_half(char *s)
+{
+int len;
+len = _strlen(s);
+
+if (len %  2 == 0)
+{
+int r = len / 2;
+int e = r;
+while (e < r + r)
+{
+_putchar(s[e]);
+e++;
+}
+}
+else
+{
+int r = len + 1 / 2;
+int e = r;
+while (e < r + (r - 1))
+{
+_putchar(s[e]);
+e++;
+}
+}
+_putchar('\n');
+}
