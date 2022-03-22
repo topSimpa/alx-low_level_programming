61a099164e7d0d635bfaa37d418c2bd08ec8a13c
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-22 00:19:04 +0000

    lets print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts_half - print half of a string
+* @s: s is the string pointer (string)
+* Return: void (success)
+*/
+
+void puts_half(char *s)
+{
+int len = _strlen(s);
+
+if (len %  2 == 0)
+{
+int r = len / 2;
+while (r < r + r)
+{
+_putchar(s[r]);
+r++;
+}
+}
+else
+{
+int r = len + 1 / 2;
+while (r < r + (r - 1))
+{
+_putchar(s[r]);
+}
+}
+_putchar('\n');
+}
+
+/**
+* _strlen - change all input to 98
+* @s: s is the pointer to an array of char (string)
+* Return: int return length of string (success)
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (1)
+{
+if (s[i] == '\0')
+{
+break;
+}
+i++;
+}
+return (i);
+}
+
