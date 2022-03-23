a44cca41bfc85675193ff52c13dc4868c1815e6c
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-23 09:24:23 +0000

    prototypes and re-engineering of strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* _strlen - change all input to 98
+* @s: s is the pointer to an array of char (string)
+* Return: int return length of string (success)
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (1)
+{
+if (s[i] == '\0')
+{
+break;
+}
+i++;
+}
+return (i);
+}
+
+
+/**
+* _strcat - copies one string to another
+* @dest: destination of copy
+* @src: variable to copy
+* Return: char (Success)
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int len = _strlen(src) + 1 + _strlen(dest);
+int i = 0;
+while (i < len)
+{
+if (i == _strlen(dest))
+{
+dest[i] = src[i];
+}
+else if (i == len - 1)
+{
+dest[i] = '\0';
+}
+i++;
+}
+return (dest);
+}
