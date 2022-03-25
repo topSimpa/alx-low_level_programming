baa1547b1c333fa7735985ef2c4b8c07c995a5dd
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-25 23:26:49 +0000

    converting all upper case to lower case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - change all input to 98
+* @s: s is the pointer to an array of char (string)
+* Return: int return length of string (success)
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (1)
+{
+if (s[i] == '\0')
+{
+break;
+}
+i++;
+}
+return (i);
+}
+
+
+/**
+* string_toupper - compares two strings
+* @s: string to turn upper
+* Return: int of subraction of last compared character (Success)
+*/
+
+char *string_toupper(char *s)
+{
+int len = _strlen(s);
+int i = 0;
+while (i < len)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+}
+i++;
+}
+return (s);
+}
