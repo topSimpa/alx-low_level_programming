1a49dbbaf9868ba99334035bf16c4b69e5cec572
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-28 21:23:19 +0000

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_memcpy - copys what in some part of a string memory into another
+*@dest: the destination string
+*@src: the source string
+*@n: the number of bit to copy
+*/
+
+
+
+char  *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
