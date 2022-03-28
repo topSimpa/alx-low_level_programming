4e8c270125b117ff71b02187b7741a01c13e2bc7
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-28 22:10:24 +0000

    finding the first occurence of a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_strchr - find the first occurence of a char
+*@s: the destination string
+*@c: char to check for
+*Return: return dest string pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+return ('\0');
+}
