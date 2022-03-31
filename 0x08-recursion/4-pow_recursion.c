2c3da496e3d9ce62908fbd9cf46de69737f6b7f2
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-31 02:31:16 +0000

    raise to power with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _pow_recursion - returns the result of x raise to power y
+* @x:base to raise to power y
+* @y:indices of x
+* Return: int is return (sucess)
+*/
+
+
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+{
+return (x * factorial(x, y-1));
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (-1);
+}
+}
