de2cb26ba47d6b9baec1e0381e62d0724fed5f68
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-03-31 02:33:38 +0000

    raise to power with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,7 +12,7 @@ int _pow_recursion(int x, int y)
 {
 if (y > 0)
 {
-return (x * factorial(x, y-1));
+return (x * _pow_recursion(x, y-1));
 }
 else if (y == 0)
 {
@@ -23,3 +23,4 @@ else
 return (-1);
 }
 }
+
