91e003076ea43965b2605be3e7edc4396e9018ba
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-01 21:26:58 +0000

    addition of number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* @argc: the count of argument
+* @argv: pointer to an array of strings passed as command line arg
+* Return: 0 int (sucess)
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int  i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+			printf("%s\n", "Error");
+			return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
