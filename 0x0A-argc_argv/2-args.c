b80bac159317384d2d46442b3962e16b13266196
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-01 19:40:56 +0000

    print all command line argument to compile file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* @argc: the count of argument
+* @argv: pointer to an array of strings passed as command line arg
+* Return: 0 int (sucess)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
