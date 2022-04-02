a6fff289ab8359011ee6c28f6bc164c80e87380b
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-02 20:40:30 +0000

    accounting for negative sign

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,29 +13,39 @@ void print_to_98(int n)
 
 if (n > 98)
 {
-while (n > 97)
-{
-write_num(n);
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-n--;
-}
+	while (n > 97)
+	{
+		write_num(n);
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n--;
+	}
 }
 else
 {
-while (n < 99)
-{
-write_num(n);
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-n++;
-}
+	while (n < 99)
+	{
+		if (n < 0)
+		{
+			int i = -1 * n;
+
+			_putchar('-');
+			write_num(i);
+		}
+		else
+		{
+			write_num(n);
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
+	}
 }
 _putchar(10);
 }
@@ -48,19 +58,19 @@ _putchar(10);
 
 void write_num(int i)
 {
-if ((i > 9 || i < -9) && (i < 100 || i > -99))
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-else if (i > 99 || i < -100)
-{
-_putchar(i / 100 + '0');
-_putchar((i / 10) % 10 + '0');
-_putchar(i % 10 + '0');
-}
-else
-{
-_putchar(i + '0');
-}
+	if (i > 9 && i < 100)
+	{
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else if (i > 99)
+	{
+		_putchar(i / 100 + '0');
+		_putchar((i / 10) % 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else
+	{
+		_putchar(i + '0');
+	}
 }
