e3a8eb380ed5327c1a051bbe53bf97c72543eeeb
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-04 16:10:27 +0000

    fibonacci even series

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,32 +6,29 @@
 * Return: int  0 (sucess)
 */
 
-int main (void)
+int main(void)
 {
 long int f = 1;
 long int s = 1;
+long int n = 0;
+long int sum = 0;
 int i = 0;
 
-while (i < 50)
+while (n <= 4000000)
 {
-	if (i == 0)
+	if (i > 0)
 	{
-		putchar('1');
-	}
-	else
-	{
-		long int n = s + f;
-		printf("%ld", n);
+		n = s + f;
+		if (n % 2 == 0)
+		{
+			sum += n;
+		}
 		f = s;
 		s = n;
 	}
-	if (i != 49)
-	{
-		putchar(',');
-		putchar(' ');
-	}
 	i++;
 }
+printf("%ld", sum);
 putchar('\n');
 return (0);
 }
