eb2914297f387b6be87b55a74e014fde99d82ad9
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-04 19:51:52 +0000

    more fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - Printing out the first 50 term of a fibonnaci sequence
+* starting  from 1 and 2
+* Return: int  0 (sucess)
+*/
+
+int main(void)
+{
+long int f = 1;
+long int s = 1;
+int i = 0;
+
+while (i < 98)
+{
+	if (i == 0)
+	{
+		putchar('1');
+	}
+	else
+	{
+		long int n = s + f;
+
+		printf("%ld", n);
+		f = s;
+		s = n;
+	}
+	if (i != 97)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
+
+
