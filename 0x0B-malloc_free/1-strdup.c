61fe83c585c575b25bbe33bd2af16b90b06ceb75
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-05 23:44:54 +0000

    a string is copied

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - copy a string
+* @str: string to copy
+* Return: a pointer to the array
+*/
+
+char *_strdup(char *str);
+{
+char *arr = malloc(sizeof(*str));
+unsigned int i = 0;
+if (str == NULL)
+{
+	return (NULL);
+}
+else if (arr != NULL)
+{
+	while (i < size)
+	{
+		arr[i] = str[i];
+		i++;
+	}
+}
+return (arr);
+}
