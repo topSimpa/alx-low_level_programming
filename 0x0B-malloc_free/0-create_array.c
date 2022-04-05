fed59659cf5631aa916b409bb8de6870a3c1c475
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-05 20:59:52 +0000

    scripts that create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * create_array - a function that create a char array
 * @size: size of array
 * @c: character to initialize it to
+* Return: a pointer to the array
 */
 
 char *create_array(unsigned int size, char c)
