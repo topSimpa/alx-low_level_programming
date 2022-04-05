2e526793e8dfd6527fad7ef8a9526cb5203c3280
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-05 00:37:38 +0000

    print a number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* write_num - to write out number
+* @i: number to print
+* Return: void (Sucess)
+*/
+
+void write_num(int i)
+{
+	if (i > 9 && i < 100)
+	{
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else if (i > 99 && i < 1000)
+	{
+		_putchar(i / 100 + '0');
+		_putchar((i / 10) % 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else if (i > 999)
+	{
+		_putchar(i / 1000 + '0');
+		_putchar((i / 100) % 10 + '0');
+		_putchar((i / 10) % 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else
+	{
+		_putchar(i + '0');
+	}
+}
+
+/**
+* print_number - To print a number
+* @n: n is the number to print
+* Return: void (Sucess)
+*/
+
+
+void print_number(int n)
+{
+if (n < 0)
+{
+	_putchar('-')
+	n = n * -1;
+}
+write_num(n);
+}
+
