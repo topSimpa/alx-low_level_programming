2b75672d10a49cae9b88250c8dc6eb19dec5d44a
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-07 01:12:09 +0000

    creating a 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 int s = 0;
 int i = 0;
 int j = 0;
-int **arr = malloc(sizeof(int) * height);
+int **arr = malloc(sizeof(int) * height * width);
 
 while (s < height)
 {
