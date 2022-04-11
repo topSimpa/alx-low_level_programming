8be880c3281b84404ac44066880c98b38c631418
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-11 21:49:49 +0000

    first struct for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+learning how to use struct in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - description of a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner the dog
+*
+* Description: All the information could be declare with the struct
+*/
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif /* for _DOG_H_ */
