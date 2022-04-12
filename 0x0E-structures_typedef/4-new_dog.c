43cce5b75cc6bb152b96a8404d5c0f246182551c
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-12 00:30:33 +0000

    creating and return new dog_t struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+*new_dog - initializes the variable in struct
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of the dog
+*Return: pointer to a struct
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t r;
+dog_t *d;
+
+r.name = name;
+r.age = age;
+r.owner = owner;
+d = &r;
+
+if (d && name && age && owner)
+{
+	return (d);
+}
+return (NULL);
+}
+
+
