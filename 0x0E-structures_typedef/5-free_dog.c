a7123d4e3f4825e590668c433d7470e81163d5ed
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-12 01:06:07 +0000

    freeing space

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - printing the dog struct
+*@d: pointer to dog
+*Return: void is return
+*/
+
+void free_dog(dog_t *d)
+{
+
+if (d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d->owner);
+}
+
+}
