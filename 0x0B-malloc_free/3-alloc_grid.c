f97c229a99cee434f477a28635c18dbb55c533e9
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-13 09:58:08 +0000

    checking my understanding of pointers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 int s = 0;
 int i = 0;
 int j = 0;
-int **arr = malloc(sizeof(int **) * height);
+int **arr = malloc(sizeof(int *) * height);
 if (arr == NULL)
 {
 	free(arr);
