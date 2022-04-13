294b7e16febfa26e704e186cd10e4f7dc9edbe7a
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-13 22:20:39 +0000

    using function pointer as an argument

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*print_name - print a name using a function called
+*@f: function pointer to style to print
+*@name: pointer to string to name to print
+*Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
