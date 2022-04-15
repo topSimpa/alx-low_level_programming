ea54ee4c9df5f6e7d582020ad121ff820a5c54f3
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-15 13:38:52 +0000

    printing n numbers of numbers using seperators and no seperators when null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ if (n)
 	va_start(op_arg, n);
 	while (i <= n)
 	{
-		if (separator)
+		if (separator ==)
 		{
 			if (i != n)
 			{
@@ -44,6 +44,7 @@ if (n)
 	va_end(op_arg);
 
 }
+
 va_end(op_arg);
 }
 
