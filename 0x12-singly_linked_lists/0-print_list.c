142bfac144f77d5cafcb6be9393770526a0ecbbf
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-22 00:44:48 +0000

    printing the values of a struct

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - print the element of singly linked list
+ * @h: pointer to the type list_t
+ *
+ * Return: the number of nodes
+ * Description: getting to print linked list of value string
+ */
+
+
+size_t print_list(const list_t *h)
+{
+
+	unsigned long int i = 0;
+
+
+	do {
+
+		if (h->str)
+		{
+			printf("[%i] ", h->len);
+			printf("%s\n", h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		i++;
+		h = h->next;
+	} while (h);
+	return (i);
+
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+/**
+ * struct list_s - singly linked 1ists
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+void _putchar(char c);
+size_t print_list(const list_t *h);
+
+
+#endif /*list.h*/
+
