2657663869ea01dbbc396eb99963b8e5d6a52daf
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-23 20:35:31 +0000

    before main comes me

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _first - print this before execution of main file
+ *
+ * Return: returns void
+ */
+
+void _first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
