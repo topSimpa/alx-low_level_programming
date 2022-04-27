c5760f5c7cab261986b7752ba3cf5c618cf642df
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-27 11:36:28 +0000

    dont know

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include "2-add_nodeint.c"
+#include "3-add_nodeint_end.c"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -21,24 +21,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	k = listint_len(*head);
 	if (head)
 	{
+		new = malloc(sizeof(listint_t));
+
+		if (new == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->n = n;
+		h = *head;
+
 		if (idx < k && idx > 0)
 		{
 			j = idx;
-			h = *head;
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				free(new);
-				return (NULL);
-			}
-
 			while (j > 0)
 			{
 				b = h;
 				h = h->next;
 				j--;
 			}
-			new->n = n;
 			b->next = new;
 			new->next = h;
 		}
@@ -48,7 +49,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else if (idx == 0)
 		{
-			add_nodeint(head, n);
+			*head = new;
+			new->next = h;
 		}
 		return (new);
 	}
