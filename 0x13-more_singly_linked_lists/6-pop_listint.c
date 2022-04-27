e8a8ff73fbc54e4f6769ae29676ad2964cc6947e
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-27 03:10:22 +0000

    pop a linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * pop_listint - adding an element to a linked list
+ * @head: a pointer to a pointer that point to head of l-lists
+ *
+ * Return: return the address or a pointer to new element
+ */
+
+
+int pop_listint(listint_t **head)
+{
+	if (head)
+	{
+		int n;
+		listint_t *new;
+
+		n  = (*head)->n;
+		new = (*head)->next;
+		head  = &new;
+		return (n);
+	}
+	return (0);
+}
+
