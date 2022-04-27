93cce683f86474ad0054209bfd0da692f3569537
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-27 11:10:31 +0000

    adding at any index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+#include "1-listint_len.c"
+#include "2-add_nodeint.c"
+#include "3-add_nodeint_end.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * insert_nodeint_at_index - adding an element to a linked list end
+ * @head: a pointer to a pointer that point to head of l-lists
+ * @idx: the index to insert
+ * @n: the new integer data to put at index
+ *
+ * Return: return the address or a pointer to new element
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *h, *new, *b;
+	unsigned int j, k;
+
+	k = listint_len(*head);
+	if (head)
+	{
+		if (idx < k && idx > 0)
+		{
+			j = idx;
+			h = *head;
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+			{
+				free(new);
+				return (NULL);
+			}
+
+			while (j > 0)
+			{
+				b = h;
+				h = h->next;
+				j--;
+			}
+			new->n = n;
+			b->next = new;
+			new->next = h;
+		}
+		else if (idx == k)
+		{
+			add_nodeint_end(head, n);
+		}
+		else if (idx == 0)
+		{
+			add_nodeint(head, n);
+		}
+		return (new);
+	}
+return (NULL);
+}
