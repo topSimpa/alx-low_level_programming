4e707f41bb28025cf57cba146f6fb6244d079460
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-04-29 02:01:58 +0000

    decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -17,7 +17,6 @@ void print_binary(unsigned long int n)
 	int e = n;
 
 	c = find_count(n);
-	/*printf("%d\n", c); */
 	for (; c >= 0; c--)
 	{
 		j = c;
@@ -26,7 +25,6 @@ void print_binary(unsigned long int n)
 		{
 			h = h * 2;
 		}
-		/*printf("%d\n", h);*/
 		if (h > e)
 		{
 			_putchar('0');
@@ -51,11 +49,11 @@ int find_count(unsigned long int n)
 	long int count = 0;
 	unsigned long int j = 1;
 
-	while(1)
+	while (1)
 	{
 		if (j * 2 > n)
 		{
-			return(count);
+			return (count);
 		}
 		j = j * 2;
 		count++;
