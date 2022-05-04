d6bb8b2a03e453e65bb46086e187e8747c883626
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-04 19:07:17 +0000

    lets read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - read from a text file
+ * @filename:  name of the file
+ * @letters: the number of character to be printed
+ *
+ * Return: the number of character sucessfully writen out
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *reads;
+	int rd, wr, op;
+
+	op = open(filename, O_RDONLY);
+	reads = malloc(letters + 1);
+
+	if (reads == NULL)
+	{
+		free(reads);
+	}
+
+	if (op != -1 && filename)
+	{
+		rd = read(op, reads, letters);
+		reads[rd] = '\0';
+
+		if (rd != -1)
+		{
+			wr = write(1, reads, rd);
+			if (wr == -1)
+			{
+				return (0);
+			}
+			return (wr);
+		}
+		close(op);
+		return (0);
+	}
+
+	return (0);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /*main.h*/
