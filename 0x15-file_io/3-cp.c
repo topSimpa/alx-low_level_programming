e15209ccd8ba921c71909fb23ab88cf29d01a926
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-05 20:11:09 +0000

    error message right

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	op1 = open(av[1], O_RDONLY);
