2d247e3c052d4c296f52ef2c77e906709d00502a
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-05 19:02:08 +0000

    till the end

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		op = open(filename, (O_CREAT | O_TRUNC) | O_WRONLY, 0600);
+		op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 		if (op != -1)
 		{
 			if (text_content)
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,38 +17,43 @@
 int main(int ac, char **av)
 {
 	char reads[1025];
-	int rd, op1, op2, wr, cl1, cl2;
+	int count = 0, rd, op1, op2, wr, cl1, cl2;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
 		exit(97);
 	}
-
 	op1 = open(av[1], O_RDONLY);
-	rd = read(op1, reads, 1024);
-	if (op1 == -1 || rd == -1)
+	while (1)
 	{
-		dprintf(2, "Error: Can't read from %s\n", av[1]);
-		exit(98);
+		rd = read(op1, reads, 1024);
+		if (op1 == -1 || rd == -1)
+		{
+			dprintf(2, "Error: Can't read from %s\n", av[1]);
+			exit(98);
+		}
+		if (rd == 0)
+			break;
+
+		reads[rd] = '\0';
+
+		if (count == 0)
+			op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
+		wr = write(op2, reads, rd);
+		if (op2 == -1 || wr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		count++;
 	}
-	reads[rd] = '\0';
 	cl1 = close(op1);
 	if (cl1 == -1)
-	{
 		dprintf(2, "Error: Can't close fd %d", cl1);
-	}
-	op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	wr = write(op2, reads, rd);
-	if (op2 == -1 || wr == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	cl2 = close(op2);
 	if (cl2 == -1)
-	{
 		dprintf(2, "Error: Can't close fd %d", cl2);
-	}
 	return (0);
 }
