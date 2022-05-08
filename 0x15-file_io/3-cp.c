c0bd47cd0d85af0255adcb987d04c0d4314e09bf
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-08 22:28:45 +0000

    end not error return sucess

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,11 +46,13 @@ int main(int ac, char **av)
 		rd = read(op1, reads, 1024);
 		if (op1 == -1 || rd == -1)
 		{
+			if (rd == -1 && op1 != -1)
+				close(op1);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		if (rd == 0 && count > 0)
-			break;
+			return (0);
 		reads[rd] = '\0';
 		if (count == 0)
 			op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
