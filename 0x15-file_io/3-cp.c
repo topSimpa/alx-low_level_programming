e15a914407b5bb298c5c5901e735bfdac2cd837b
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-06 07:57:02 +0000

    create if file one exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ void close_error(int fd)
 int main(int ac, char **av)
 {
 	char reads[1025];
-	int count = 0, rd, op1, op2, wr, cl1, cl2;
+	int rd, op1, op2, wr, cl1, cl2;
 
 	if (ac != 3)
 	{
@@ -43,28 +43,28 @@ int main(int ac, char **av)
 	op1 = open(av[1], O_RDONLY);
 	if (op1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	while (1)
+
+
+	op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (op2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
+
+	while ((rd = read(op1, reads, 1024)) > 0)
 	{
-		rd = read(op1, reads, 1024);
-		if (rd == -1)
-		{
-			cl1 = close(op1);
-			if (cl1 == -1)
-				close_error(op1);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		}
-		if (rd == 0 && count > 0)
-			break;
 		reads[rd] = '\0';
-		if (count == 0)
-			op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
 		wr = write(op2, reads, rd);
-		if (op2 == -1 || wr == -1)
+		if (wr != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
-		count++;
 	}
+
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+
 	cl1 = close(op1);
 	if (cl1 == -1)
 		close_error(op1);
