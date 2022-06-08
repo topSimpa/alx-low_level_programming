e1986f485e13d14f67b4ad403ec11756fa9eb9cf
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-08 07:39:19 +0000

    malloc revisited

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked
+ *
+ * @b: size of the memory to allocat
+ * Return; void()
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *allocated;
+
+	allocated = malloc(b);
+	if (allocated == NULL)
+	{
+		free(allocated);
+		exit(98);
+	}
+
+	return (allocated);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void *malloc_checked(unsigned int b);
+
+
+#endif /* _MAIN_H */
