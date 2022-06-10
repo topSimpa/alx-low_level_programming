b5035deec687514727cdd95c0dc871730950ae02
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-10 20:59:50 +0000

    looking like pro using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_len - length of a string
+ * @c: pointer to the string
+ *
+ * Return: int of length
+ */
+
+
+unsigned int str_len(char *c)
+{
+	unsigned int len = 0;
+
+	while (c[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * string_nconcat - joins a str and n
+ * @s1: the first strings
+ * @s2: the second string
+ * @n: the number of char from the second string
+ *
+ * Return: a string concated
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1, len_s2, len_sn, ind = 0, s = 0;
+	char *copy;
+
+	len_s1 = str_len(s1);
+	len_s2 = str_len(s2);
+
+	if (n >= len_s2)
+		n = len_s2;
+
+	len_sn = n + len_s1;
+	copy = malloc(len_s1 + n + 1);
+	if (copy == NULL)
+		free(copy);
+	for (; ind < len_sn; ind++)
+	{
+		if (ind < len_s1)
+			copy[ind] = s1[ind];
+		else
+			copy[ind] = s2[s++];
+	}
+	copy[ind] = '\0';
+	return (copy);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,6 @@
 #define _MAIN_H
 
 void *malloc_checked(unsigned int b);
-
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+unsigned int str_len(char *c);
 #endif /* _MAIN_H */
