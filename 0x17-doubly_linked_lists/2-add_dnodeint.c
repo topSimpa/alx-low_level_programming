ef93d7a7ce44ae47121fb4fe29e7cac82d16af8c
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-15 18:41:31 +0000

    every node has a pair of pointers on it left and right

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - add an integer to the begining
+ * @n: the integer to add at the front
+ * @head: pointer to the head of the dlist
+ *
+ * Return: the address of the new int
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->prev = NULL;
+	new->n = n;
+	if (*head)
+	{
+		(*head)->prev = new;
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	return (new);
+}
+
