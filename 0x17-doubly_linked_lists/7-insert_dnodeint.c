4b951c2d51d668c283e3d0e881b502195fdd49a0
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-16 19:17:33 +0000

    it should also add to end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (idx == 0)
 			new = add_dnodeint(h, n);
 		else if (idx == dlistint_len(*h))
-			new = add_add_dnodeint_end(h, n);
+			new = add_dnodeint_end(h, n);
 		else
 		{
 			new->n = n;
