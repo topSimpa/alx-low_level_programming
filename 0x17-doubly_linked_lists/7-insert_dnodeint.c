d82644e601cad334001679a5576da6d10ed31d81
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-16 18:34:17 +0000

    take care of empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			free(new);
 			return (NULL);
 		}
-		if (idx == 0)
+		if (idx == 0 || *h == NULL)
 			new = add_dnodeint(h, n);
 		else
 		{
