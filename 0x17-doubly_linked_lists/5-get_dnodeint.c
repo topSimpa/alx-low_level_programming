d48aec51a68fa9aaf42df30d5c2530ec9a6e6fd8
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-16 12:16:13 +0000

    getting node is just a loop

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - frees a dlist
+ *@head: the first node
+ *@index: the index
+ *
+ *Return: node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *h;
+
+	if (head != NULL && index < dlistint_len(head))
+	{
+
+		h = head;
+		do {
+			h = h->next;
+		} while ((--index) > 0);
+		return (h);
+	}
+	return (NULL);
+}
+
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -12,6 +12,7 @@
 int main(void)
 {
     dlistint_t *head;
+    dlistint_t *node;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -23,6 +24,8 @@ int main(void)
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
+    node = get_dnodeint_at_index(head, 5);
+    printf("%d\n", node->n);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
