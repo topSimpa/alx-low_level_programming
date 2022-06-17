65d3c9f66c875bdccd7cecf5dea154606043137f
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-17 11:54:34 +0000

    psuedocode works, delete does not free

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "1-dlistint_len.c"
+
+/**
+ * delete_dnodeint_at_index - delete desired node
+ * @head: the pointer to the first head
+ * @index: the index of node
+ *
+ *Return: return 1 sucess and -1 failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *h;
+	unsigned int loop = 0;
+
+	if (*head && index < dlistint_len(*head))
+	{
+		if (dlistint_len(*head) == 1)
+		{
+			*head = NULL;
+			return (1);
+		}
+
+		if (index == 0)
+		{
+			temp = (*head)->next;
+			temp->prev = NULL;
+			*head = temp;
+			return (1);
+		}
+
+		h = *head;
+		for (; loop < index; loop++)
+			h = h->next;
+
+		if (index == dlistint_len(*head) - 1)
+		{
+			h->prev->next = NULL;
+			return (1);
+		}
+
+		else
+		{
+			temp = h->next;
+			temp->prev = h->prev;
+			h->prev->next = temp;
+			return (1);
+		}
+	}
+	return (-1);
+}
