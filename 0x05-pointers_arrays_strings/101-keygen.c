df02056e3c4320d6f163233f6068017935060f9e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-05 02:01:33 +0000

    learnt the use of time and srand

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <time.h>
 
 int main(void)
 {
-	printf("abcdefghijklmnopqrstuvwxy/");
-	return (0);
+	char num;
+	int pass = 0;
+
+	srand(time(NULL));
+	while(1)
+	{
+		num = (rand() % 128) + 1;
+		pass += num;
+		if (pass == 2772)
+		{
+			putchar(num);
+			return (0);
+		}
+		if (pass > 2772)
+		{
+			pass = pass - num;
+			num = 2772 - pass;
+			putchar(num);
+			return (0);
+		}
+		putchar(num);
+	}
 }
