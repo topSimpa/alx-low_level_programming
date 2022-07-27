22d19bbd618cbe29f4fd655bebe27c2a1dd0dd7f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-27 21:31:42 +0000

    a 2X2 matrix

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_chessboard - print a model of a chessboard
+ *
+ * @a: the array that represent the chessboard
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int n = 0;
+	unsigned int i;
+
+	while (n < 8)
+	{
+		if (n > 1 && n < 6)
+			putchar('\n');
+		else
+		{
+			i = 0;
+			while (i < 8)
+			{
+				putchar(a[n][i]);
+				if (i == 7)
+					putchar('\n');
+				i++;
+			}
+		}
+		n++;
+	}
+}
