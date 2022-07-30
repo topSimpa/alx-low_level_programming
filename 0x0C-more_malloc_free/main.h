85c0c4954a23f6d4c5e691b228971824f01316d7
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-30 19:54:44 +0000

    realloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int str_len(char *c);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void simple_print_buffer(int *buffer, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size);
 #endif /* _MAIN_H */
