9e097bbd8463245fe8f5380a023826efb4528dd1
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-05 17:44:20 +0000

    added linear search algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches an array for a value linearly
+ * @array: array to search through
+ * @size: length of the specified array
+ * @value: the value to find
+ *
+ * Return: index where value is found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, *(array));
+		if (*array == value)
+			return (i);
+		i++;
+		array++;
+	}
+}
+
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* main.h */
