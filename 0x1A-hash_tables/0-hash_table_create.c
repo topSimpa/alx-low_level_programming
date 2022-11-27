30c627076c69f03fb1c21fe36206dc14570d9e41
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-27 22:16:46 +0000

    added the size value

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,6 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
+	new_table->size = size;
 	new_table->array = calloc(size, sizeof(hash_node_t **));
 	if (new_table->array == NULL)
 	{
