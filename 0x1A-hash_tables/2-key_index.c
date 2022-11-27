8cd0bf8876ce8cae12c4161cd4ba3064cb20639e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-27 22:41:49 +0000

    added the key generator function

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - generate the index of a key
+ *
+ * @key: the key to use to generate an index
+ * @size of the array to store the key and value
+ *
+ * Return: the index generate by function
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return	(hash_djb2(key) % size);
+}
