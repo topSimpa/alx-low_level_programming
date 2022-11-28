039f5bd6351b639f1c46e99fe9b9d0cff3857939
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-28 08:30:31 +0000

    no operation if table is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new;
+	if (ht == NULL)
+		return (0);
 
 	new = malloc(sizeof(hash_node_t *));
 	if (new == NULL)
