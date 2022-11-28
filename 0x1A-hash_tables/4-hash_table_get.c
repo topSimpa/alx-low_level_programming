cb7fb4ff267136382c9d09123913cc76719d55ce
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-28 21:11:53 +0000

    added function to get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value
+ *
+ * @ht: the hash table
+ * @key: the key to the hash code
+ *
+ * Return: char pointer to the value
+ */
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *h;
+	unsigned int count = 0;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	h = h->array[index];
+	while (h != NULL)
+	{
+		if (streq(h->key, (char *)key) == 0)
+			break;
+		h = h->next;
+		count++;
+	}
+	if (count == 0)
+		return (NULL);
+	return (h->value);
+}
